3df87091ef13c8bcaf01a6ada31f935e51e1cd60
Yayo_joaco <gjrabanal@gmail.com>
2025-06-06 20:10:39 -0500

    hola

---
+++ src/main/java/com/example/lab8_20204205/Lab820204205Application.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20204205;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab820204205Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab820204205Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab8_20204205/Lab820204205ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab8_20204205;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab820204205ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
